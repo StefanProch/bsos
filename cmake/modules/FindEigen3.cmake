b9e22ccb18c5f9be1d423eed09fb592ec9be67e4
StefanProch <stefan.proch@int.pl>
2026-06-14 20:04:28 +0200

    .

--- cmake/modules/FindEigen3.cmake
+++ cmake/modules/FindEigen3.cmake
@@ -10,15 +10,43 @@ find_path(EIGEN3_INCLUDE_DIR
     PATH_SUFFIXES include/eigen3 include/eigen eigen3 eigen
 )
 
-if(EIGEN3_INCLUDE_DIR)
-    file(READ "${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h" _eigen3_version_header)
-    string(REGEX MATCH "define[ \t]+EIGEN_WORLD_VERSION[ \t]+([0-9]+)" _eigen3_world_version_match "${_eigen3_version_header}")
-    set(EIGEN3_WORLD_VERSION "${CMAKE_MATCH_1}")
-    string(REGEX MATCH "define[ \t]+EIGEN_MAJOR_VERSION[ \t]+([0-9]+)" _eigen3_major_version_match "${_eigen3_version_header}")
-    set(EIGEN3_MAJOR_VERSION "${CMAKE_MATCH_1}")
-    string(REGEX MATCH "define[ \t]+EIGEN_MINOR_VERSION[ \t]+([0-9]+)" _eigen3_minor_version_match "${_eigen3_version_header}")
-    set(EIGEN3_MINOR_VERSION "${CMAKE_MATCH_1}")
+set(_eigen3_version_header "")
+foreach(_candidate
+    "${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h"
+    "${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/ConfigureVectorization.h"
+    "${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/Constants.h"
+)
+    if(EIGEN3_INCLUDE_DIR AND EXISTS "${_candidate}")
+        file(READ "${_candidate}" _candidate_content)
+        string(APPEND _eigen3_version_header "\n${_candidate_content}")
+    endif()
+endforeach()
+
+if(_eigen3_version_header)
+    string(REGEX MATCH "#[ \t]*define[ \t]+EIGEN_WORLD_VERSION[ \t]+([0-9]+)" _eigen3_world_version_match "${_eigen3_version_header}")
+    if(_eigen3_world_version_match)
+        set(EIGEN3_WORLD_VERSION "${CMAKE_MATCH_1}")
+    endif()
+
+    string(REGEX MATCH "#[ \t]*define[ \t]+EIGEN_MAJOR_VERSION[ \t]+([0-9]+)" _eigen3_major_version_match "${_eigen3_version_header}")
+    if(_eigen3_major_version_match)
+        set(EIGEN3_MAJOR_VERSION "${CMAKE_MATCH_1}")
+    endif()
+
+    string(REGEX MATCH "#[ \t]*define[ \t]+EIGEN_MINOR_VERSION[ \t]+([0-9]+)" _eigen3_minor_version_match "${_eigen3_version_header}")
+    if(_eigen3_minor_version_match)
+        set(EIGEN3_MINOR_VERSION "${CMAKE_MATCH_1}")
+    endif()
+endif()
+
+if(DEFINED EIGEN3_WORLD_VERSION AND DEFINED EIGEN3_MAJOR_VERSION AND DEFINED EIGEN3_MINOR_VERSION)
     set(Eigen3_VERSION "${EIGEN3_WORLD_VERSION}.${EIGEN3_MAJOR_VERSION}.${EIGEN3_MINOR_VERSION}")
+    set(EIGEN3_VERSION "${Eigen3_VERSION}")
+elseif(EIGEN3_INCLUDE_DIR)
+    # The CI dependency recipe builds Eigen 5.0.1. Some Eigen snapshots do not expose
+    # the version macros in the historical header path used by older find modules.
+    # Do not fail with a bogus ".." version when the dependency include tree exists.
+    set(Eigen3_VERSION "5.0.1")
     set(EIGEN3_VERSION "${Eigen3_VERSION}")
 endif()
 
